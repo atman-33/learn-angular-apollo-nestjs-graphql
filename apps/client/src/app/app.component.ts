25b951e1b418778f5e3462defc8144cdcfea2ff3
atman-33 <gpbjk0304@gmail.com>
2023-06-17 14:21:43 +0900

    add angular

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'learn-angular-apollo-nestjs-graphql-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
