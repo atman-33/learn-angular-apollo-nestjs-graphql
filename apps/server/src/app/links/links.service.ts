c2ad92017facebf195e7d2d6b256574634294a7f
atman-33 <gpbjk0304@gmail.com>
2023-06-19 23:44:14 +0900

    improve getLinks to add retry feature

--- apps/server/src/app/links/links.service.ts
+++ apps/server/src/app/links/links.service.ts
@@ -5,9 +5,31 @@ import { GetLinksArgs } from './dto/args/get-links-args.dto';
 @Injectable()
 export class LinksService {
     async getLinks(getLinksArgs: GetLinksArgs) {
+        const retryCount = 3;
+        const retryDelay = 1000;
+
         return Promise.all(
             getLinksArgs.urls.map(async (url) => {
-                return getLinkPreview(url);
+                let retry = 0;
+                let linkPreview = null;
+
+                while (retry < retryCount) {
+                    try {
+                        linkPreview = await getLinkPreview(url);
+                        break;
+                    } catch (error) {
+                        if (error.message !== 'Request timeout') {
+                            throw error;
+                        }
+
+                        retry++;
+                        if (retry >= retryCount) {
+                            throw error;
+                        }
+                        await new Promise((resolve) => setTimeout(resolve, retryDelay));
+                    }
+                }
+                return linkPreview;
             })
         );
     }
